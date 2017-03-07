32571ecec619747679ca184f8dadad73971c0c28
sarleon <sarleon56@hotmail.com>
2017-03-07 22:29:38 +0800

    make Logger singleton

--- core/craber.py
+++ core/craber.py
@@ -1,3 +1,3 @@
 
 class craber:
-    self
+    pass
--- log/Logger.py
+++ log/Logger.py
@@ -1,7 +1,21 @@
+import time
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
 class Logger:
 
+    __metaclass__ = 'Singleton'
+
     log_levels=["FATAL","ERROR","WARNING","DEBUG","INFO"]
+
+    def __repr__(self):
+         return "[Logger]"
+
     def __init__(self,log_level,log_fileoath="spider.log"):
         self.log_level=log_level or 0
         self.log_filepath=log_fileoath
@@ -10,9 +24,10 @@ class Logger:
 
     def log(self,level,infomation):
         infomation_prefix="[%s]" % self.log_levels[level]
-        infomation=infomation_prefix+infomation
-        if(level<=self.log_level):
+        if level<=self.log_level:
+            current_time=time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime())
+            self.log_file.write(infomation_prefix+current_time+infomation)
 
-            self.log_file.write()
+
 
 
